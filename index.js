e108582d618787c330b0a8043a57d0008963b26b
Lucas Rollin <lrollin@enssat.fr>
2019-05-29 14:48:37 +0200

    added gitignore

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 http.createServer((request, response) => {
 
 	response.writeHead(200);
-	response.end('Hello World!');
+	response.end('Hello World!\n');
 
 }).listen(8080);
 
