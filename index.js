19e86e0cd5e827ce00f922ee88189f7854b4c268
Lucas Rollin <lrollin@enssat.fr>
2019-05-29 14:45:31 +0200

    http server on port 8080

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('coucou');
+const http = require('http');
+
+
+
+http.createServer((request, response) => {
+
+	response.writeHead(200);
+	response.end('Hello World!');
+
+}).listen(8080);
+
